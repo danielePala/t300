1594b550f738bb4bda818e8f5678736cb779a19b
Daniele Pala <pala.daniele@gmail.com>
2019-10-22 12:30:15 +0200

    more template processing

--- t300.go
+++ t300.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-	"log"
-	//"strings"
-	"io/ioutil"
 	"flag"
-	"strconv"
-	"text/template"
-	"os"
+	"fmt"
 	copy "github.com/otiai10/copy"
 	"github.com/tealeg/xlsx"
-	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"text/template"
 )
 
 // Configuration data for a single RTU
 type RTUConf struct {
-	Name string
+	Name          string
 	StreetAddress string
 	CommonAddress int
-	IP string
-	Netmask string
-	DefaultGW string
-	SNTPServer string
-	ProtConfs []ProtConf
+	IP            string
+	Netmask       string
+	DefaultGW     string
+	SNTPServer    string
+	ProtConfs     []ProtConf
 }
 
 // Configuration data for a single protection
 type ProtConf struct {
-	Name string
-	Num int
-	IP string
-	Netmask string
-	DefaultGW string
+	Name       string
+	Num        int
+	IP         string
+	Netmask    string
+	DefaultGW  string
 	Affacciata string
 }
 
@@ -39,9 +38,9 @@ func main() {
 	// array of configurations from input file
 	configurations := make(map[string]*RTUConf)
 	// Template projects
-	confDirs := map[int]string {
+	confDirs := map[int]string{
 		1: "Config_1_REF_V3_TEMPLATE",
-		2: "Config_2_REF_V3_TEMPLATE", 
+		2: "Config_2_REF_V3_TEMPLATE",
 		3: "Config_3_REF_V3_TEMPLATE",
 	}
 	// input configuration file
@@ -49,7 +48,7 @@ func main() {
 	// template directory path
 	var templatePath = flag.String("tmpl", ".", "Path to template projects")
 	flag.Parse()
-	
+
 	// parse the configuration file
 	xlFile, err := xlsx.OpenFile(*conf)
 	if err != nil {
@@ -58,22 +57,25 @@ func main() {
 	// RTU sheet
 	rtuSheet := xlFile.Sheet["RTU"]
 	for i, row := range rtuSheet.Rows {
-		newConf := RTUConf {};
+		newConf := RTUConf{}
 		// skip header line
 		if i == 0 {
 			continue
 		}
+		if len(row.Cells[0].String()) == 0 {
+			break
+		}
 		for i, cell := range row.Cells {
 			switch i {
 			case 0:
-				newConf.Name = cell.String() 
+				newConf.Name = cell.String()
 			case 1:
 				newConf.StreetAddress = cell.String()
 			case 2:
 				ca, err := strconv.Atoi(cell.String())
 				if err != nil {
 					continue
-				}	
+				}
 				newConf.CommonAddress = ca
 			case 3:
 				newConf.IP = cell.String()
@@ -86,15 +88,19 @@ func main() {
 			}
 		}
 		configurations[newConf.Name] = &newConf
-        }
+		fmt.Println("Added new RTU: ", newConf.Name)
+	}
 	// PROTEZIONI sheet
 	protSheet := xlFile.Sheet["PROTEZIONI"]
 	for i, row := range protSheet.Rows {
 		var rtu *RTUConf
-		newProt := ProtConf {};
+		newProt := ProtConf{}
 		// skip header line
 		if i == 0 {
 			continue
+		}
+		if len(row.Cells[0].String()) == 0 {
+			break
 		}
 		for i, cell := range row.Cells {
 			switch i {
@@ -118,37 +124,57 @@ func main() {
 				newProt.Affacciata = cell.String()
 			}
 		}
+		fmt.Printf("Added new Protection %s to RTU %s\n", newProt.Name, rtu.Name)
 		rtu.ProtConfs = append(rtu.ProtConfs, newProt)
-        }
+	}
 	// for each configuration generate the project from template
 	for _, configuration := range configurations {
 		tmplDir := confDirs[len(configuration.ProtConfs)]
-		err = copy.Copy(*templatePath + "/" + tmplDir, configuration.Name)
+		err = copy.Copy(*templatePath+"/"+tmplDir, configuration.Name)
 		if err != nil {
 			log.Fatal(err)
 		}
+		prjPath := configuration.Name + "/" + configuration.Name
+		prjFilesPath := prjPath + " Files"
+		// rename paths according to generated project
+		err = os.Rename(configuration.Name+"/"+tmplDir+".ctpx", prjPath+".ctpx")
+		err = os.Rename(configuration.Name+"/"+tmplDir+" Files", prjFilesPath)
+
 		// parse Profile.xml
-		profilePath := configuration.Name + "/" + tmplDir + " Files" + "/Profile.xml"
-		data, err := ioutil.ReadFile(profilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		profileFile, err := os.OpenFile(profilePath, os.O_WRONLY, 0644) 
-		if err != nil {
-			log.Fatal(err)
-		}
-		tmpl, err := template.New("test").Parse(string(data))
-		if err != nil {
-			log.Fatal(err)
-		}
-		
-		err = tmpl.Execute(profileFile, configuration)
-		if err != nil {
-			log.Fatal(err)
-		}
-		// close the file
-		if err := profileFile.Close(); err != nil {
-			log.Fatal(err)
-		}
+		profilePath := prjFilesPath + "/Profile.xml"
+		parseTemplate(profilePath, configuration)
+		// parse T300_61850.scd
+		scdPath := prjFilesPath + "/i61sc/T300_61850.scd"
+		parseTemplate(scdPath, configuration)
+		// parse i4e_cont.xml
+		i4ePath := prjFilesPath + "/i4e/i4e_cont.xml"
+		parseTemplate(i4ePath, configuration)
+		// parse thmConf.xml
+		thmPath := prjFilesPath + "/thmConf.xml"
+		parseTemplate(thmPath, configuration)
+	}
+}
+
+func parseTemplate(fileName string, configuration *RTUConf) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.OpenFile(fileName, os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl, err := template.New("test").Parse(string(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tmpl.Execute(file, configuration)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// close the file
+	if err := file.Close(); err != nil {
+		log.Fatal(err)
 	}
 }
